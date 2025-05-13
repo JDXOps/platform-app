f93d2cdb5b3d487b3951a537e671d36f40bcb373
Jasdeep Singh <jsd@Jasdeeps-MacBook-Pro.local>
2025-05-13 14:28:15 +0100

    feat(app): add jdxops name to demo app

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    message = "Flask app running on Kubernetes =)!"
+    message = "JDXOps app running on Kubernetes =)!"
     return f"<h1>{message}</h1>"
 
 
