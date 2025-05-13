fd5c846266d882b52be76a9f866947f05635fac9
jdial1996 <42215471+jdial1996@users.noreply.github.com>
2025-05-13 15:25:55 +0100

    feat(app): remove emoji from demo app landing page (#10)
    * feat(app): remove emoji from demo app landing page
    
    * add correct secret name for dockerhub credentials
    
    ---------
    
    Co-authored-by: Jasdeep Singh <jsd@Jasdeeps-MacBook-Pro.local>

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    message = "JDXOps demo app running on K8s! =)"
+    message = "JDXOps demo app running on K8s!"
     return f"<h1>{message}</h1>"
 
 
